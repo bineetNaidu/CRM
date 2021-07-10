24a43f3b523f6cc34e39e78b70caecbf074d7b5f
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-10 14:24:39 +0530

    🚀 clean ups

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>CRM | bineetnaidu.io</title>
+        <meta name="description" content="A Personal CRM by Bineet Naidu" />
+        <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta
+          name="keywords"
+          content="Bineet Naidu, Bineet, bineetnaidu, crm, CRM, bineet projects"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
